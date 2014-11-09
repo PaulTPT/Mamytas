ca81352bdae5f96bbafe3755823b6483f289142b
Paul BERTHIER <paul.berthier@outlook.com>
2014-11-09 12:18:10 -0500

    Ajout des differentes ressources

---
+++ app/src/main/java/mn/aug/restfulandroid/provider/RemindersDBAccess.java
@@ -0,0 +1,7 @@
+package mn.aug.restfulandroid.provider;
+
+/**
+ * Created by Paul on 09/11/2014.
+ */
+public class RemindersDBAccess {
+}
---
+++ app/src/main/java/mn/aug/restfulandroid/rest/resource/Reminder.java
@@ -0,0 +1,7 @@
+package mn.aug.restfulandroid.rest.resource;
+
+/**
+ * Created by Paul on 09/11/2014.
+ */
+public class Reminder {
+}
